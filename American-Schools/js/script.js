52c29e79ecb7ec24e039f46503c6d4a23f8af6b5
Leonardo0o <168543710+DasStilleKind@users.noreply.github.com>
2024-12-19 14:15:20 +0100

    Add files via upload

---
+++ American-Schools/js/script.js
@@ -0,0 +1,5 @@
+// JavaScript to display more information when button is clicked
+function showSchoolType() {
+    const description = document.getElementById("school-type-description");
+    description.textContent = "In the U.S., public schools are funded by local taxes, while private schools are independently funded. Charter schools are a mix of both, with a focus on specific educational methods.";
+}
